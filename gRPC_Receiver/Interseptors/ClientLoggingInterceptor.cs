0cca273f3575b201f95863de75b1d577b844603c
Roman Kirillov <RKirillov@live.ru>
2025-02-22 23:16:00 +0300

    fix: interceptors

---
+++ gRPC_Receiver/Interseptors/ClientLoggingInterceptor.cs
@@ -0,0 +1,47 @@
+﻿
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+
+namespace gRPC_Receiver.Interceptors
+{
+    public class ClientLoggingInterceptor : Interceptor
+    {
+        private readonly ILogger<ClientLoggingInterceptor> _logger;
+
+        public ClientLoggingInterceptor(ILogger<ClientLoggingInterceptor> logger)
+        {
+            _logger = logger;
+        }
+
+        // Override the AsyncUnaryCall method to intercept unary calls
+        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+            TRequest request,
+            ClientInterceptorContext<TRequest, TResponse> context,
+            AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+            _logger.LogInformation("Starting call. Type/Method: {Type} / {Method}",
+                context.Method.Type, context.Method.Name);
+            return continuation(request, context);
+        }
+
+        public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(
+        TRequest request,
+        ClientInterceptorContext<TRequest, TResponse> context,
+        AsyncServerStreamingCallContinuation<TRequest, TResponse> continuation)
+            {
+                _logger.LogInformation("Starting call. Type/Method: {Type} / {Method}",
+                    context.Method.Type, context.Method.Name);
+                return continuation(request, context);
+            }
+
+
+        public override AsyncClientStreamingCall<TRequest, TResponse> AsyncClientStreamingCall <TRequest, TResponse>(
+        ClientInterceptorContext<TRequest, TResponse> context,
+        AsyncClientStreamingCallContinuation<TRequest, TResponse> continuation)
+        {
+            _logger.LogInformation("Starting call. Type/Method: {Type} / {Method}",
+                context.Method.Type, context.Method.Name);
+            return continuation( context);
+        }
+    }
+}
--- gRPC_Receiver/Service/ReceiverService.cs
+++ gRPC_Receiver/Service/ReceiverService.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using Grpc.Core;
-using Grpc.Net.Client;
 using gRPC_Receiver.Entity;
 using GrpcServices;
+using System;
 using System.Threading.Channels;
 
 namespace gRPC_Receiver.Service
@@ -10,17 +10,18 @@ namespace gRPC_Receiver.Service
     public class ReceiverService : IReceiverService
     {
         private readonly ILogger<ReceiverService> _logger;
+/*        In your ReceiverService class, the StreamEntities method is invoked on _senderServiceClient, which is an instance of SenderService.SenderServiceClient.This indicates that ReceiverService is acting as a client in this context, making requests to a gRPC server.*/
         private readonly SenderService.SenderServiceClient _senderServiceClient;
         private readonly IMapper _mapper;
         private readonly Channel<AdkuEntity> _channel;
         private int entityCounter = 0;
-        public ReceiverService(ILogger<ReceiverService> logger, IMapper mapper)
+        public ReceiverService(ILogger<ReceiverService> logger, IMapper mapper, SenderService.SenderServiceClient senderServiceClient)
         {
             _logger = logger;
             _mapper = mapper;
 
             // Создание ограниченного канала с емкостью 10000
-            _channel = Channel.CreateBounded<AdkuEntity>(new BoundedChannelOptions(30000)
+            _channel = System.Threading.Channels.Channel.CreateBounded<AdkuEntity>(new BoundedChannelOptions(30000)
             {
                 FullMode = BoundedChannelFullMode.Wait, // Блокирует запись, если канал переполнен
                 SingleReader = true, // Разрешает только один поток для чтения
@@ -28,19 +29,22 @@ namespace gRPC_Receiver.Service
             });
 
             // Подключение к серверу gRPC (порт 5000, например)
-            var grpcServerAddress = "http://localhost:5000"; // Адрес сервера gRPC
-            var grpcChannel = GrpcChannel.ForAddress(grpcServerAddress);
+            /*            var grpcServerAddress = "http://localhost:5000"; // Адрес сервера gRPC
+                        var grpcChannel = GrpcChannel.ForAddress(grpcServerAddress);
 
-            _senderServiceClient = new SenderService.SenderServiceClient(grpcChannel);
+                        _senderServiceClient = new SenderService.SenderServiceClient(grpcChannel);*/
+            _senderServiceClient = senderServiceClient;
         }
 
         public async Task ReceiveEntitiesAsync(CancellationToken cancellationToken)
         {
             var request = new ReceiverRequest { RequestId = Guid.NewGuid().ToString() };
-            using var call = _senderServiceClient.StreamEntities(request);
+            //Represents an asynchronous server-streaming call that returns multiple GrpcServices.Entity messages.
+            using AsyncServerStreamingCall<GrpcServices.Entity>? call = _senderServiceClient.StreamEntities(request);
 
             try
             {
+                //call.ResponseStream: An asynchronous stream of GrpcServices.Entity messages sent by the server.
                 await foreach (var grpcEntity in call.ResponseStream.ReadAllAsync(cancellationToken))
                 {
                     var adkuEntity = _mapper.Map<AdkuEntity>(grpcEntity);
