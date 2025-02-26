d86bc2ddd531aad5fdb51477960603a3602d2475
Roman Kirillov <RKirillov@live.ru>
2025-02-26 17:33:24 +0300

    fix: added bearer jwt

---
+++ gRPC_Receiver/JWT/TokenProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace gRPC_Receiver.JWT
+{
+    public interface ITokenProvider
+    {
+        string GetToken(CancellationToken cancellationToken);
+    }
+
+    public class AppTokenProvider : ITokenProvider
+    {
+        private string? _token;
+
+        public string GetToken(CancellationToken cancellationToken)
+        {
+            if (_token == null)
+            {
+                // App code to resolve the token here.
+                _token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InRlc3QxIiwibmJmIjoxNTgxOTYyNzI0LCJleHAiOjE1ODE5NjYzMjQsImlhdCI6MTU4MTk2MjcyNH0.VvYln0PgZQrFwBTx0Ik3TGGI43DxdVVxzHAXma-K5P0";
+            }
+
+            return _token;
+        }
+    }
+}
--- gRPC_Receiver/Program.cs
+++ gRPC_Receiver/Program.cs
@@ -4,7 +4,8 @@ using Grpc.Net.Client;
 using GrpcServices;
 using gRPC_Receiver.Service;
 using gRPC_Receiver.Mapper;
-using gRPC_Receiver.Interceptors; // Пространство имен для вашего gRPC сервиса
+using gRPC_Receiver.Interseptors;
+using gRPC_Receiver.JWT; // Пространство имен для вашего gRPC сервиса
 
 // Регистрация сервисов
 var builder = WebApplication.CreateBuilder(args);
@@ -13,13 +14,25 @@ builder.Services.AddSingleton<IReceiverService, ReceiverService>();
 builder.Services.AddSingleton<IChannelService, ChannelService>();
 builder.Services.AddAutoMapper(typeof(EntityMappingProfile));
 builder.Services.AddHostedService<ReceiverServiceWithTimer>(); // Регистрация фонового сервиса
-
+builder.Services.AddScoped<ITokenProvider, AppTokenProvider>();
 builder.Services.AddSingleton<ClientLoggingInterceptor>();
 
 builder.Services.AddGrpcClient<SenderService.SenderServiceClient>(options =>
 {
     options.Address = new Uri("http://localhost:5000"); // Укажите адрес вашего gRPC-сервера
 })
+        //Bearer token with gRPC client factory
+        .AddCallCredentials((context, metadata, serviceProvider) =>
+        {
+            // Получаем сервис из DI контейнера для получения токена
+            var provider = serviceProvider.GetRequiredService<ITokenProvider>();
+            var _token =  provider.GetToken(context.CancellationToken);
+            if (!string.IsNullOrEmpty(_token))
+            {
+                metadata.Add("Authorization", $"Bearer {_token}");
+            }
+            return Task.CompletedTask;
+        })
 .AddInterceptor<ClientLoggingInterceptor>(); // Добавляем интерсептор;
 
 // Настройка Swagger (если используется)
@@ -38,3 +51,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+
